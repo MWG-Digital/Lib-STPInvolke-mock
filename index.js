61fc2bec32c6e8ea71f2fe88e43b119df6135097
Israel Melicio <israel@mwg.digital>
2021-03-29 10:58:09 -0300

    Initial commit diff --git a/index.js b/index.js new file mode 100644 index 0000000..6a35fad --- /dev/null +++ b/index.js @@ -0,0 +1,17 @@ +'use strict' + +class Console { +  static info (descricao, item) { +    console.info(descricao, item) +  } + +  static payload (descricao, item) { +    console.info(descricao, item) +  } + +  static error (descricao, item) { +    console.info(descricao, item) +  } +} + +module.exports.Console = Console diff --git a/package.json b/package.json new file mode 100644 index 0000000..fca3d54 --- /dev/null +++ b/package.json @@ -0,0 +1,25 @@ +{ +  "name": "libStpInvoke", +  "version": "2.0.0", +  "description": "Lib nodejs para chamada de qualquer componente externo ao lambda", +  "main": "index.js", +  "scripts": {}, + +  "repository": {}, +  "dependencies": { +    "xml2js": "*", +    "util": "*" +  }, +  "devDependencies": { +    "http": "*", +    "https": "*", +    "aws-sdk": "*" +  }, +  "keywords": [ +    "node.js", +    "eclipse", +    "nodeclipse" +  ], +  "author": "Marco Tulio", +  "license": "MIT" +}

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class Console {
+  static info (descricao, item) {
+    console.info(descricao, item)
+  }
+
+  static payload (descricao, item) {
+    console.info(descricao, item)
+  }
+
+  static error (descricao, item) {
+    console.info(descricao, item)
+  }
+}
+
+module.exports.Console = Console
